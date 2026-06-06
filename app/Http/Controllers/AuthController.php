7a02dd8a13b29120b461e05ce284adddda74a506
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-06-06 20:32:54 -0300

    login e criação de token p acesso a api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+
+        //validate request
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        //check credentials
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $attempt = auth()->attempt([
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if (!$attempt) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        // authenticate user
+        $user = auth()->user();
+        $token = $user->createToken($user->name)->plainTextToken;
+
+        // return the access token
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+}
